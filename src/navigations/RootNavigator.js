23dcf84438c20800d4ec011ae92c467b0a6c2b7e
Saket <saketk89@gmail.com>
2025-09-16 01:06:25 +0530

    feat - started with DNS app

---
+++ src/navigations/RootNavigator.js
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+
+const RootNavigator = () => {
+  return (
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default RootNavigator;
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './style'
+
+const LoginScreen = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Text>LoginScreen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
