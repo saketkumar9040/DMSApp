83299bd09a247bcda223cf6c8c334680957a5937
Saket <saketk89@gmail.com>
2025-09-17 00:54:37 +0530

    feat - added a date picker

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,150 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator, Modal, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+import styles from './style'
+import { Colors } from '../../globals/Colors'
+import { FontSizes } from '../../globals/FontSizes'
 
 const HomeScreen = ({ navigation, route }: any) => {
 
+  const dispatch = useDispatch()
+
   const store_user_data = useSelector((state: any) => state.main);
-  
   console.log("store_user_data", store_user_data);
 
+  const [open_upload_modal, set_open_upload_modal] = useState(false)
+  const [open_search_modal, set_open_search_modal] = useState(false)
+  const [upload_document_pressed, set_upload_document_pressed] = useState(false);
+  const [search_document_pressed, set_search_document_pressed] = useState(false);
+
+  const [date, set_date] = useState("");
+  const [open_upload_date_picker, set_open_upload_date_picker] = useState(false)
+
+  const upload_documemnt_handler = async () => {
+    try {
+
+    } catch (error) {
+      console.log("Error while uploading document =========> ", error)
+    }
+  };
+
+  const search_documemnt_handler = async () => {
+    try {
+
+    } catch (error) {
+      console.log("Error while uploading document =========> ", error)
+    }
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.mainContainer}>
+      {/* ===========================   Search modal     =========================== */}
+      {
+        open_search_modal && (
+          <Modal
+            animationType="slide"
+            transparent
+            visible={open_search_modal}
+            onRequestClose={() => set_open_search_modal(false)}
+          >
+            <View
+              style={styles.overlay}
+            >
+              <View style={{ ...styles.modalContainer, flex: 1, backgroundColor: Colors.bg, paddingHorizontal: 0 }}>
+                <TouchableOpacity style={{ position: "absolute", top: 10, right: 10, zIndex: 1 }} onPress={() => set_open_search_modal(false)}>
+                  <Text style={styles.closeText}>close</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        )
+      }
+
+      {/* ===========================   Upload modal     =========================== */}
+      {
+        open_upload_modal && (
+          <Modal
+            animationType="slide"
+            transparent
+            visible={open_upload_modal}
+            onRequestClose={() => set_open_upload_modal(false)}
+          >
+            <View
+              style={styles.overlay}
+            >
+              <View style={{ ...styles.modalContainer, flex: 1, backgroundColor: Colors.bg, padding: 15 }}>
+                <TouchableOpacity style={{ alignSelf: "flex-end" }} onPress={() => set_open_upload_modal(false)}>
+                  <Text style={styles.closeText}>close</Text>
+                </TouchableOpacity>
+                <View style={{ flex: 1, paddingVertical: 30, paddingHorizontal: 20 }}>
+                  <View style={{ flexDirection: "row", alignItems: "center", justifyContent: 'space-between' }}>
+                    <Text style={styles.titleHeading}>Pick A Date</Text>
+                    <TouchableOpacity style={styles.titleTextInput} onPress={() => set_open_upload_date_picker(true)}>
+                      <Text style={styles.titleHeading}>{date || "Enter Date"}</Text>
+                    </TouchableOpacity>
+
+                    {open_upload_date_picker && (
+                      <DateTimePicker
+                        value={date ? new Date(date) : new Date()}
+                        mode={"date"}
+                        display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                        onChange={(event: any, selectedDate?: any) => {
+                          if (event.type === "dismissed" || !selectedDate) {
+                            set_open_upload_date_picker(false);
+                            return;
+                          }
+                          console.log("Selected date", JSON.stringify(selectedDate))
+                          set_date(new Intl.DateTimeFormat("en-GB").format(new Date(selectedDate)).replace(/\//g, "-"))
+                          set_open_upload_date_picker(false)
+                        }}
+                      />
+                    )}
+                  </View>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        )
+      }
+
+      {/* ===============================     Main Home Screen     ===================================== */}
+      <View style={styles.loginContainer}>
+        <Image
+          source={require("../../assets/images/docs.png")}
+          style={styles.docsImage}
+          resizeMode='cover'
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.generateOTPButton}
+          activeOpacity={0.7}
+          disabled={upload_document_pressed}
+          onPress={() => {
+            set_open_upload_modal(true)
+          }}
+        >
+          <Text style={styles.generateOTPText}>
+            Upload
+          </Text>
+
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.searchButton}
+          activeOpacity={0.7}
+          disabled={search_document_pressed}
+          onPress={() => {
+            set_open_search_modal(true)
+          }}
+        >
+          <Text style={styles.searchText}>
+            Search
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/style.tsx
+++ src/screens/Home/style.tsx
@@ -1,7 +1,115 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { Colors } from "../../globals/Colors";
+import { FontSizes } from "../../globals/FontSizes";
+
+const { width, height } = Dimensions.get("window")
 
 const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: Colors.bg
+    },
+    loginContainer: {
+        marginTop: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 50,
+    },
+    loginText: {
+        fontSize: FontSizes.font26,
+        color: Colors.black,
+        fontWeight: "600",
+        textAlign: "center"
+    },
+    textInputContainer: {
+        marginHorizontal: 20
+    },
+    generateOTPText: {
+        fontSize: FontSizes.font20,
+        color: Colors.bg,
+        fontWeight: "500",
+        textAlign: "center"
+    },
+    buttonContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    generateOTPButton: {
+        flexDirection: "row",
+        alignSelf: "center",
+        padding: 10,
+        paddingHorizontal: 30,
+        backgroundColor: Colors.teal,
+        marginVertical: 20,
+        borderRadius: 50,
+        width: width * 0.5,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    searchButton: {
+        flexDirection: "row",
+        alignSelf: "center",
+        padding: 10,
+        paddingHorizontal: 30,
+        borderWidth: 1,
+        borderColor: Colors.teal,
+        marginVertical: 20,
+        borderRadius: 50,
+        width: width * 0.5,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    searchText: {
+        fontSize: FontSizes.font20,
+        color: Colors.teal,
+        fontWeight: "500",
+        textAlign: "center"
+    },
+    docsImage: {
+        height: 120,
+        width: 120,
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: Colors.grey,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+    },
 
-});
+    // ======================== Modal Style ==============================//
+    overlay: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        backgroundColor: Colors.black + 50, // Optional dark background
+    },
+    modalContainer: {
+        backgroundColor: Colors.bg,
+        paddingHorizontal: 20,
+        // paddingTop: 5,
+    },
+    closeText: {
+        fontSize: FontSizes.font20,
+        fontWeight: "700",
+        textDecorationLine: "underline",
+        color: Colors.teal
+    },
+    titleHeading: {
+        fontSize: FontSizes.font18,
+        fontWeight: "500",
+        color: Colors.black
+    },
+    titleTextInput: {
+        borderWidth: 1,
+        borderColor: Colors.grey,
+        borderRadius: 10,
+        paddingHorizontal: 15,
+        padding: 5,
+        width: 150,
+        elevation: 10,
+        backgroundColor:Colors.bg
+    }
+})
 
 export default styles;
